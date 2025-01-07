c2b3cb7dda216eda048998ed1b4f12226187db6c
AYUSH KUMAR GUPTA <ayushkumargupta2908@gmail.com>
2025-01-07 19:31:09 +0530

    shadcn components additions

---
+++ app/sign-up/page.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useSignUp } from '@clerk/nextjs'
+import { useRouter } from 'next/navigation'
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+  
+
+
+function Signup() {
+    return (
+        <div>
+            ad
+        </div>
+    )
+}
+
+export default Signup
